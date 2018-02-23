a515e96ecb531a3f7dc8ca817583d42e86316de3
Stefano Degenkamp <stefanodegenkamp@gmail.com>
2018-02-23 16:49:09 +0100

    Cocktail view added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Martini")
+Cocktail.create(name: "Margarita")
+Cocktail.create(name: "Gin-tonic")
+
