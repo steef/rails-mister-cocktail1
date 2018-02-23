64274c7aeea87ad5f98821359b940f6e9dcfb5ae
Stefano Degenkamp <stefanodegenkamp@gmail.com>
2018-02-23 12:14:35 +0100

    Doses controller bug fix

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -25,6 +25,6 @@ class DosesController < ApplicationController
   private
 
   def dose_params
-    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
